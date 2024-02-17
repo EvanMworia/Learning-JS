619e938f14c8a0fd617cf5545f5ede8df252072c
EvanMworia <evannji99@gmail.com>
2024-02-17 16:04:13 +0300

    Initial Commit

---
+++ 0x001-Learning-Functions/app.js
@@ -0,0 +1,10 @@
+function sayHello(name) {
+  console.log("Hello there" + " " + name);
+}
+
+let person1 = "NANCY";
+let person2 = "EDF";
+let person3 = "JAMES";
+sayHello(person1);
+sayHello(person2);
+sayHello(person3);
